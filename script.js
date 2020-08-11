151ebb347966523c93876fda030b5bb35d079e9b
Timdot <68854765+Timdot@users.noreply.github.com>
2020-08-11 13:12:39 +0100

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// alert('welcome!')
+//     /*Toggle mobile menu*/
+
+// function toggleMenu() {
+//     if (menu.classList.contains(active)) {
+//         /*adds the hamburger icon*/
+//         toggle.querySelector("a").innerHTML = '<I class="fa fa-bars"></i>'
+//     }else{
+//             menu.classList.add("active")
+//         }
+// }
+
+/*Toggle mobile menu*/
